798b636845ca5cea13e7089cf942b10c25d7037a
DPAHiK <artiom.medvedev2005@yandex.ru>
2025-01-13 20:00:03 +0300

    styled the login page

--- front-end/src/components/Login.js
+++ front-end/src/components/Login.js
@@ -28,29 +28,54 @@ const Login = () => {
   };
 
   return (
-    <div className='container'>
-      <Error body={errorBody}/>
+  <div className="container py-5 h-100">
+    <Error body={errorBody}/>
+    <div className="row d-flex justify-content-center align-items-center h-100">
+      <div className="col-12 col-md-8 col-lg-6 col-xl-5">
+        <div className="card bg-dark text-white" style={{borderRadius: 1}}>
+          <div className="card-body p-5 text-center">
 
-      <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        placeholder="Name"
-        className='form-control'
-        required
-      />
-            <input
-        type="text"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-        className='form-control'
-        required
-      />
-      <button type="submit" className='btn btn-success'>Login</button>
-    </form>
+            <div className="mb-md-5 mt-md-4 pb-5">
+
+              <h2 className="fw-bold mb-2 text-uppercase">Login</h2>
+              <form onSubmit={handleSubmit}>
+                <div data-mdb-input-init className="form-outline form-white mb-4">
+                  <label className="form-label" for="typeEmailX">Name</label>
+                  <input
+                  id="typeEmailX"
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  placeholder="Name"
+                  className='form-control form-control-lg'
+                  required
+                  />
+                </div>
+
+                <div data-mdb-input-init className="form-outline form-white mb-4">
+                 <label className="form-label" for="typePasswordX">Password</label>
+                  <input
+                  id="typePasswordX"
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  placeholder="Password"
+                  className='form-control form-control-lg'
+                  required
+                  />
+                </div>
+
+
+                <button data-mdb-button-init data-mdb-ripple-init className="btn btn-outline-light btn-lg px-5" type="submit">Login</button>
+
+              </form>
+            </div>
+
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
   )
 
 };
