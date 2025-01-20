924fa12fc6147cedf9cd64cffe975b6f922a36ad
DPAHiK <artiom.medvedev2005@yandex.ru>
2025-01-20 17:55:07 +0300

    changed a view og tournament info page a little

--- front-end/src/components/TournamentInfo.js
+++ front-end/src/components/TournamentInfo.js
@@ -28,11 +28,20 @@ const TournamentInfo = () => {
   const lowGridMatches = matches.filter(match => match.grid_level == 1)
   const lastMatches = matches.filter(match => match.grid_level == 0)
 
-  matches.sort((a, b) => {
-    const aDate = new Date(a.start_date)
-    const bDate = new Date(b.start_date)
-    //console.log(aDate.getTime() - bDate.getTime())
-    return aDate.getTime() - bDate.getTime()})
+  function setTeamNameColor(isTeam1Winner, teamNumber){
+    //console.log(isTeamWinner)
+    if(isTeam1Winner === null)return "mb-0 text-center"
+    if(isTeam1Winner === true && teamNumber === 1) return "mb-0 text-center text-success"
+    return "mb-0 text-center text-danger"
+
+    
+  }
+
+  // matches.sort((a, b) => {
+  //   const aDate = new Date(a.start_date)
+  //   const bDate = new Date(b.start_date)
+  //   //console.log(aDate.getTime() - bDate.getTime())
+  //   return aDate.getTime() - bDate.getTime()})
 //    console.log(params.id)
     // console.log(engagedTeams)
     // console.log(matches)
@@ -52,16 +61,65 @@ const TournamentInfo = () => {
         <h2>Matches</h2>
         <div className='container mt-5'>
           <div className='row'>
-            {matches && matches.map(match => (
+            {highGridMatches[0] && <div className='align-self-center col-md-auto '>High grid:</div>}
+            {highGridMatches && highGridMatches.map(match => (
             <div className='col col-lg-2 border border-secondary mx-5' key={match.id}>
-              <h4 className="mb-0 text-center">
+              <h4 className={setTeamNameColor(match.is_team1_winner, 1)}>
               {engagedTeams.find(team => team.team_id == match.team1_id) &&
               engagedTeams.find(team => team.team_id == match.team1_id).name}
               </h4>
 
               <h6 className="my-2 text-center">vs</h6>
 
-              <h4 className="mb-0 text-center">
+              <h4 className={setTeamNameColor(match.is_team1_winner, 2)}>
+              {engagedTeams.find(team => team.team_id == match.team2_id) &&
+              engagedTeams.find(team => team.team_id == match.team2_id).name}
+              </h4>
+
+              <p className="mt-3 text-center">{match.start_date}</p>
+            
+            </div>
+            ))}
+          </div>
+
+          <div className='row'>
+            {lastMatches[0] && <div className='align-self-center col-md-auto '>Final:</div>}
+            {highGridMatches && highGridMatches.map(match => (
+            <div className='col col-lg-2 border-white border-secondary mx-5' key={match.id}/>
+            ))}
+
+            {lastMatches && lastMatches.map(match => (
+            <div className='col col-lg-2 border border-secondary mx-5 my-5' key={match.id}>
+              <h4 className={setTeamNameColor(match.is_team1_winner, 1)}>
+              {engagedTeams.find(team => team.team_id == match.team1_id) &&
+              engagedTeams.find(team => team.team_id == match.team1_id).name}
+              </h4>
+
+              <h6 className="my-2 text-center">vs</h6>
+
+              <h4 className={setTeamNameColor(match.is_team1_winner, 2)}>
+              {engagedTeams.find(team => team.team_id == match.team2_id) &&
+              engagedTeams.find(team => team.team_id == match.team2_id).name}
+              </h4>
+
+              <p className="mt-3 text-center">{match.start_date}</p>
+            
+            </div>
+            ))}
+          </div>
+
+          <div className='row'>
+            {lowGridMatches[0] && <div className='align-self-center col-md-auto '>Low grid:</div>}
+            {lowGridMatches && lowGridMatches.map(match => (
+            <div className='col col-lg-2 border border-secondary mx-5' key={match.id}>
+              <h4 className={setTeamNameColor(match.is_team1_winner, 1)}>
+              {engagedTeams.find(team => team.team_id == match.team1_id) &&
+              engagedTeams.find(team => team.team_id == match.team1_id).name}
+              </h4>
+
+              <h6 className="my-2 text-center">vs</h6>
+
+              <h4 className={setTeamNameColor(match.is_team1_winner, 2)}>
               {engagedTeams.find(team => team.team_id == match.team2_id) &&
               engagedTeams.find(team => team.team_id == match.team2_id).name}
               </h4>
